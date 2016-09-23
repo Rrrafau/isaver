45a8408c4459d51fc265123c0b12f464a4b21a83
Rrrafau <radolasd@gmail.com>
2016-09-24 06:43:14 +0800

    demo

--- components/AddSpendings.js
+++ components/AddSpendings.js
@@ -165,7 +165,7 @@ export default class AddSpendings extends Component {
       <div>
         <div className="isaver-header">
           <div className="container">
-          <h1><i className="fa fa-balance-scale fa-lg"></i> Add your expenses (DEMO)</h1>
+          <h1><i className="fa fa-balance-scale fa-lg"></i> Add your expenses <b>(DEMO)</b></h1>
           </div>
         </div>
         <div className="container">

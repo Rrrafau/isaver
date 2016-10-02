aef46bf558b881078cb64897132d9853b3fcc28f
Rrrafau <radolasd@gmail.com>
2016-10-03 04:08:40 +0800

    dp fix for safari

--- actions/spendings.js
+++ actions/spendings.js
@@ -16,7 +16,7 @@ let GraphQLEndpoint = 'http://localhost:3001/api'
 
 // console.log(process.env.NODE_ENV, (process.env.NODE_ENV === 'production'))
 // if (process.env.NODE_ENV === 'production') {
-  // GraphQLEndpoint = 'http://isaver.online/api'
+  GraphQLEndpoint = 'http://isaver.online/api'
 // }
 
 function getSpendings(variables) {
